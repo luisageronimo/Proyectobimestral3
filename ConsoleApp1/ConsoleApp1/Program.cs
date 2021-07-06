b8fc2c62f95b9baf65d9d82488862ff9892cfa63
Luisa Gerónimo <luisageronimo5@gmail.com>
2021-07-06 13:58:53 -0600

    Proyecto Bimestral 3 terminado

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion2
+{
+    class ProyectoBimestral
+    {
+        static void Main(string[] args)
+        {
+            int val1 = 0;
+            int val2 = 0;
+            int total = 0;
+            char op = 's';
+
+            while (op == 's')
+            {
+
+                Console.Write("Ingrese su peso en libras actual: ");
+                val1 = int.Parse(Console.ReadLine());
+                int total1 = (val1 * 703);
+
+                Console.Write("Ingrese su estatura en pulgadas: ");
+                val2 = int.Parse(Console.ReadLine());
+                int total2 = (val2 * val2);
+
+                Console.WriteLine("Su indice de masa corporal es de: " + total1 / total2);
+
+
+                Console.WriteLine("Desea realizar otro ['s/n'] ");
+                op = char.Parse(Console.ReadLine());
+                Console.Clear();
+            }
+            Console.WriteLine("Fin");
+            Console.ReadKey();
+
+        }
+    }
+}
